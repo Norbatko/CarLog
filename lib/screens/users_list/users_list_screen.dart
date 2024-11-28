7afa11336cf24f7a3702634bb10c622dd898516b
Tomáš Ebert <tomasebert@Mac.home>
2024-11-28 22:50:30 +0100

    feat: Refactor list into widget + sort users

--- lib/screens/users_list/users_list_screen.dart
+++ lib/screens/users_list/users_list_screen.dart
@@ -1,6 +1,7 @@
 import 'package:car_log/model/user.dart';
 import 'package:car_log/screens/users_list/widgets/user_tile_widget.dart';
 import 'package:car_log/services/Routes.dart';
+import 'package:car_log/screens/users_list/widgets/users_list.dart';
 import 'package:car_log/set_up_locator.dart';
 import 'package:car_log/widgets/builders/build_future_with_stream.dart';
 import 'package:car_log/widgets/theme/app_bar.dart';
@@ -17,13 +18,16 @@ class UsersListScreen extends StatelessWidget {
     final UserService userService = get<UserService>();
 
     return Scaffold(
-      appBar: ApplicationBar(title: 'User List', userDetailRoute: Routes.userDetail),
+      appBar: ApplicationBar(
+          title: 'User List', userDetailRoute: Routes.userDetail),
       body: buildFutureWithStream<User?, List<User>>(
         future: _loadCurrentUser(authService, userService),
         stream: userService.users,
         loadingWidget: const Center(child: CircularProgressIndicator()),
-        errorWidget: (error) => Center(child: Text('Error loading data: $error')),
-        onData: (context, currentUser, users) => _buildBodyContent(context, currentUser, users),
+        errorWidget: (error) =>
+            Center(child: Text('Error loading data: $error')),
+        onData: (context, currentUser, users) => UsersList(
+            users: users, currentUser: currentUser, userService: userService),
       ),
     );
   }
@@ -35,25 +39,5 @@ class UsersListScreen extends StatelessWidget {
       return await userService.getUserData(user.id);
     }
     return null;
-  }
-
-  Widget _buildBodyContent(BuildContext context, User? currentUser, List<User> users) {
-    if (users.isEmpty) {
-      return const Center(child: Text('No users found'));
-    }
-
-    if (currentUser != null) {
-      users.removeWhere((user) => user.id == currentUser.id);
-    }
-    return ListView.builder(
-      itemCount: users.length,
-      itemBuilder: (context, index) {
-        final user = users[index];
-        return UserTileWidget(
-          user: user,
-          onNavigate: () => Navigator.pushNamed(context, '/user/detail', arguments: user.id),
-        );
-      },
-    );
   }
 }
---
+++ lib/screens/users_list/widgets/users_list.dart
@@ -0,0 +1,59 @@
+import 'package:car_log/model/car.dart';
+import 'package:car_log/model/user.dart';
+import 'package:car_log/screens/users_list/widgets/user_tile_widget.dart';
+import 'package:car_log/services/database_service.dart';
+import 'package:car_log/services/user_service.dart';
+import 'package:flutter/material.dart';
+
+class UsersList extends StatefulWidget {
+  final List<User> users;
+  final User? currentUser;
+  final UserService userService;
+
+  const UsersList({
+    super.key,
+    required this.users,
+    this.currentUser,
+    required this.userService,
+  });
+
+  @override
+  State<UsersList> createState() => _UsersListState();
+}
+
+class _UsersListState extends State<UsersList> {
+  late List<User> sortedUsers;
+
+  @override
+  void initState() {
+    super.initState();
+    _sortUsers();
+  }
+
+  void _sortUsers() {
+    final adminUsers = widget.users.where((user) => user.isAdmin).toList();
+    final otherUsers = widget.users.where((user) => !user.isAdmin).toList();
+    sortedUsers = adminUsers..addAll(otherUsers);
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    _sortUsers();
+    if (widget.currentUser != null) {
+      sortedUsers.removeWhere((user) => user.id == widget.currentUser!.id);
+    }
+    return sortedUsers.isEmpty
+        ? const Center(child: Text('No users available'))
+        : ListView.builder(
+            itemCount: sortedUsers.length,
+            itemBuilder: (context, index) {
+              final user = sortedUsers[index];
+              return UserTileWidget(
+                user: user,
+                onNavigate: () => Navigator.pushNamed(context, '/user/detail',
+                    arguments: user.id),
+              );
+            },
+          );
+  }
+}
