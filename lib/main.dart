ea532a2dab7e32bc759165ec979ddcd38c4071ba
Tomáš Ebert <tomasebert@Mac.home>
2024-11-29 18:40:52 +0100

    feat: Add car animation screen

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,8 @@
+import 'package:car_log/screens/car_ride/car_ride_screen.dart';
+import 'package:car_log/screens/login/animated_car_screen.dart';
 import 'package:car_log/screens/user_detail/user_detail_screen.dart';
 import 'package:car_log/set_up_locator.dart';
+import 'package:car_log/widgets/car_tab_manager.dart';
 import 'package:car_log/widgets/theme/theme_setter.dart';
 import 'package:flutter/material.dart';
 import 'package:firebase_core/firebase_core.dart';
@@ -22,17 +25,17 @@ class MyApp extends StatelessWidget {
     return AnimatedBuilder(
         animation: themeProvider,
         builder: (context, _) {
-          return
-            MaterialApp(
-              title: 'CarLog',
-              theme: themeProvider.themeData,
-              initialRoute: Routes.login,
-              routes: {
-                Routes.login: (context) => LoginScreen(),
-                Routes.userDetail: (context) => const UserDetailScreen(),
-              },
-            );
-        }
-    );
+          return MaterialApp(
+            title: 'CarLog',
+            theme: themeProvider.themeData,
+            initialRoute: Routes.login,
+            routes: {
+              Routes.login: (context) => LoginScreen(),
+              Routes.carAnimation: (context) => AnimatedCarScreen(),
+              Routes.userDetail: (context) => const UserDetailScreen(),
+              Routes.carDetail: (context) => CarTabManager(),
+            },
+          );
+        });
   }
 }
