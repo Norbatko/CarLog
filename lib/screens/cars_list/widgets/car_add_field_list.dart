9513a8439491392e537f19499e530d4ad351fa03
Tomáš Ebert <tomasebert@Mac.home>
2024-11-30 14:55:43 +0100

    feat: Extract the adding fields list into separete file

---
+++ lib/screens/cars_list/widgets/car_add_field_list.dart
@@ -0,0 +1,67 @@
+import 'package:car_log/screens/cars_list/widgets/car_add_field.dart';
+import 'package:car_log/screens/cars_list/widgets/fuel_type_dropdown.dart';
+import 'package:flutter/material.dart';
+
+class CarAddFieldList extends StatelessWidget {
+  final Map<String, TextEditingController> controllers;
+  final Map<String, String?> errorMessages;
+  final List<String> fuelTypes;
+  final String selectedFuelType;
+  final List<IconData> carIcons;
+  final int selectedCarIcon;
+  final ValueChanged<String?> onFuelTypeChanged;
+  final ValueChanged<int?> onCarIconChanged;
+
+  const CarAddFieldList({
+    super.key,
+    required this.controllers,
+    required this.errorMessages,
+    required this.fuelTypes,
+    required this.selectedFuelType,
+    required this.carIcons,
+    required this.selectedCarIcon,
+    required this.onFuelTypeChanged,
+    required this.onCarIconChanged,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return SingleChildScrollView(
+      child: Column(
+        mainAxisSize: MainAxisSize.min,
+        children: [
+          ...controllers.entries.map((entry) {
+            return CarAddField(
+              controller: entry.value,
+              errorMessage: errorMessages[entry.key],
+              nameOfField: entry.key,
+            );
+          }).toList(),
+          FuelTypeDropdown(
+            selectedFuelType: selectedFuelType,
+            fuelTypes: fuelTypes,
+            onChanged: onFuelTypeChanged,
+          ),
+          InputDecorator(
+            decoration: const InputDecoration(labelText: 'Car Icon'),
+            child: Row(
+              children: List.generate(carIcons.length, (index) {
+                return Row(
+                  mainAxisSize: MainAxisSize.min,
+                  children: [
+                    Radio(
+                      value: index,
+                      groupValue: selectedCarIcon,
+                      onChanged: onCarIconChanged,
+                    ),
+                    Icon(carIcons[index]),
+                  ],
+                );
+              }),
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
