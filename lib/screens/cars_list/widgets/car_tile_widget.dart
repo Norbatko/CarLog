fd81a4ce49567fcc8a18c3d1e308aaa622897aed
adamgabry <adam.gabrys@seznam.cz>
2024-12-03 18:48:23 +0100

    feat: refactored and decomposed car_tile_widget

--- lib/screens/cars_list/widgets/car_tile_widget.dart
+++ lib/screens/cars_list/widgets/car_tile_widget.dart
@@ -1,14 +1,14 @@
+import 'package:car_log/screens/cars_list/widgets/car_details_widget.dart';
+import 'package:car_log/screens/cars_list/widgets/car_icon_widget.dart';
+import 'package:car_log/screens/cars_list/widgets/favorite_button_widget.dart';
 import 'package:flutter/material.dart';
 import 'package:car_log/model/car.dart';
 
-const _CAR_NAME_FONT_SIZE = 16.0;
-const _LICENSE_PLATE_FONT_SIZE = 14.0;
-const _ICON_SIZE = 36.0;
 const _BORDER_RADIUS = 16.0;
 const _MARGIN = 8.0;
 const _PADDING = 12.0;
 
-  class CarTileWidget extends StatelessWidget {
+class CarTileWidget extends StatelessWidget {
   final Car car;
   final bool isFavorite;
   final VoidCallback onToggleFavorite;
@@ -35,35 +35,12 @@ const _PADDING = 12.0;
         border: Border.all(color: theme.colorScheme.primary),
       ),
       child: ListTile(
-        leading: Icon(Icons.car_rental,
-            color: theme.colorScheme.secondary, size: _ICON_SIZE),
-        title: Column(
-          crossAxisAlignment: CrossAxisAlignment.start,
-          children: [
-            Text(
-              car.name,
-              style: TextStyle(
-                fontWeight: FontWeight.bold,
-                fontSize: _CAR_NAME_FONT_SIZE,
-                color: theme.colorScheme.onSecondaryContainer,
-              ),
-            ),
-            Text(
-              car.licensePlate,
-              style: TextStyle(
-                fontSize: _LICENSE_PLATE_FONT_SIZE,
-                color: theme.colorScheme.onSecondaryContainer,
-              ),
-            ),
-          ],
-        ),
+        leading: CarIconWidget(),
+        title: CarDetailsWidget(car: car),
         onTap: onNavigate,
-        trailing: IconButton(
-          icon: Icon(
-            isFavorite ? Icons.favorite : Icons.favorite_border,
-            color: isFavorite ? Colors.red : Colors.grey,
-          ),
-          onPressed: onToggleFavorite,
+        trailing: FavoriteButtonWidget(
+          isFavorite: isFavorite,
+          onToggleFavorite: onToggleFavorite,
         ),
       ),
     );
