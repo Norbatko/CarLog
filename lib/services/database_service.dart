950a7a5628ee0e04bc1809f46d0e74b5bb0f8eb0
adamgabry <adam.gabrys@seznam.cz>
2024-12-04 13:49:38 +0100

    feat: redone to use streams at db_service and to use fireStore

--- lib/services/database_service.dart
+++ lib/services/database_service.dart
@@ -1,71 +1,94 @@
-import 'package:firebase_database/firebase_database.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:car_log/model/user.dart';
 
 class DatabaseService {
-  final DatabaseReference _userRef = FirebaseDatabase.instance.ref('users');
+  final CollectionReference _userRef = FirebaseFirestore.instance.collection('users');
 
-  Future<User?> getUserById(String uid) async {
-    try {
-      // Access the user node directly by uid
-      final snapshot = await _userRef.child(uid).get();
+  Stream<User?> getUserStreamById(String uid) {
+    return _userRef.doc(uid).snapshots().map((snapshot) {
       if (snapshot.exists) {
-        // Cast the data to Map and use uid as the local id
-        final userData = Map<String, dynamic>.from(snapshot.value as Map);
+        final userData = snapshot.data() as Map<String, dynamic>;
         return User.fromMap(uid, userData);
-      } else {
-        print("No user data found for UID: $uid");
-        return null;
       }
-    } catch (e) {
-      throw Exception('Error fetching user data: $e');
-    }
+      return null;
+    });
   }
 
-  Future<void> createUserProfile(User user) async {
+  Stream<List<User>> getAllUsersStream() {
+    return _userRef.snapshots().map((snapshot) {
+      return snapshot.docs.map((doc) {
+        final userData = doc.data() as Map<String, dynamic>;
+        return User.fromMap(doc.id, userData);
+      }).toList();
+    });
+  }
+
+  Stream<void> createUserProfile(User user) async* {
     try {
-      await _userRef.child(user.id).set(user.toMap());
+      await _userRef.doc(user.id).set(user.toMap());
+      yield null;
     } catch (e) {
       throw Exception('Error creating user profile: $e');
     }
   }
 
-  Future<void> updateUserProfile(String uid, Map<String, dynamic> updates) async {
-    if (updates.isEmpty) return; // No updates to apply
+  Stream<void> updateUserProfile(String uid, Map<String, dynamic> updates) async* {
     try {
-      await _userRef.child(uid).update(updates);
+      await _userRef.doc(uid).update(updates);
+      yield null;
     } catch (e) {
       throw Exception('Error updating user profile: $e');
     }
   }
 
-  Future<void> updateUserFavorites(String uid, List<String> favoriteCars) async {
+  Stream<void> deleteUserProfile(String uid) async* {
     try {
-      await _userRef.child(uid).update({'favoriteCars': favoriteCars});
-    } catch (e) {
-      throw Exception('Error updating favorite cars: $e');
-    }
-  }
-
-  Future<bool> isAdmin(String uid) async {
-    try {
-      final snapshot = await _userRef.child(uid).child('isAdmin').get();
-      if (snapshot.exists) {
-        return snapshot.value as bool;
-      } else {
-        print("Admin status not found for UID: $uid");
-        return false;
-      }
-    } catch (e) {
-      throw Exception('Error checking admin status: $e');
-    }
-  }
-
-  Future<void> deleteUserProfile(String uid) async {
-    try {
-      await _userRef.child(uid).remove();
-      print("User profile deleted for UID: $uid");
+      await _userRef.doc(uid).delete();
+      yield null;
     } catch (e) {
       throw Exception('Error deleting user profile: $e');
+    }
+  }
+
+  Future<User?> getUserById(String uid) async {
+    final snapshot = await _userRef.doc(uid).get();
+    if (snapshot.exists) {
+      final userData = snapshot.data() as Map<String, dynamic>;
+      return User.fromMap(uid, userData);
+    }
+    return null;
+  }
+
+  Stream<List<String>> userFavoritesStream(String uid) {
+    return _userRef.doc(uid).snapshots().map((snapshot) {
+      final data = snapshot.data() as Map<String, dynamic>?;
+      if (data != null && data['favoriteCars'] is List) {
+        return List<String>.from(data['favoriteCars']);
+      }
+      return <String>[];
+    }).handleError((error) {
+      throw Exception('Error streaming user favorites: $error');
+    });
+  }
+
+  Stream<bool> isAdminStream(String uid) {
+    return _userRef.doc(uid).snapshots().map((snapshot) {
+      final data = snapshot.data() as Map<String, dynamic>?;
+      if (data != null && data.containsKey('isAdmin')) {
+        return data['isAdmin'] as bool;
+      }
+      return false; // Default to non-admin if field is not found
+    }).handleError((error) {
+      throw Exception('Error streaming admin status: $error');
+    });
+  }
+
+  Stream<void> updateUserFavorites(String uid, List<String> favoriteCars) async* {
+    try {
+      await _userRef.doc(uid).update({'favoriteCars': favoriteCars});
+      yield null; // Emit an event to indicate completion
+    } catch (e) {
+      throw Exception('Error updating favorite cars: $e');
     }
   }
 }
