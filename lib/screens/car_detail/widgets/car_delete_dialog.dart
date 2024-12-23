98467089af91ba5102bcca91712314b1d3df4963
Tomáš Ebert <xebert@fi.muni.cz>
2024-12-23 22:01:29 +0100

    feat: Delete animation in dialog

--- lib/screens/car_detail/widgets/car_delete_dialog.dart
+++ lib/screens/car_detail/widgets/car_delete_dialog.dart
@@ -2,6 +2,10 @@ import 'package:car_log/services/Routes.dart';
 import 'package:car_log/services/car_service.dart';
 import 'package:car_log/set_up_locator.dart';
 import 'package:flutter/material.dart';
+import 'package:lottie/lottie.dart';
+
+const _ANIMATION_WIDTH = 250.0;
+const _ANIMATION_HEIGHT = 250.0;
 
 class CarDeleteDialog extends StatefulWidget {
   const CarDeleteDialog({super.key});
@@ -10,22 +14,36 @@ class CarDeleteDialog extends StatefulWidget {
   State<CarDeleteDialog> createState() => _CarDeleteDialogState();
 }
 
-class _CarDeleteDialogState extends State<CarDeleteDialog> {
+class _CarDeleteDialogState extends State<CarDeleteDialog>
+    with TickerProviderStateMixin {
   final CarService carService = get<CarService>();
   bool _isDeleting = false;
+  late final AnimationController _controller;
+
+  @override
+  void initState() {
+    super.initState();
+    _controller = AnimationController(vsync: this);
+  }
+
+  @override
+  void dispose() {
+    _controller.dispose();
+    super.dispose();
+  }
 
   @override
   Widget build(BuildContext context) {
     return ElevatedButton(
-      onPressed: () {
-        // Navigator.of(context).pop();
-        // carService.deleteCar(carService.activeCar.id).listen((_) {});
-        _showDeleteCarDialog(context);
-      },
+      onPressed: () => _showDeleteCarDialog(context),
       style: ElevatedButton.styleFrom(
-          backgroundColor: Colors.red,
-          textStyle: TextStyle(color: Colors.black)),
-      child: Text("Delete Car"),
+        backgroundColor: Colors.red,
+        textStyle: const TextStyle(color: Colors.black),
+      ),
+      child: const Text(
+        "Delete Car",
+        style: TextStyle(color: Colors.black),
+      ),
     );
   }
 
@@ -47,46 +65,78 @@ class _CarDeleteDialogState extends State<CarDeleteDialog> {
   }
 
   Widget _buildDialogTitle() {
-    return _isDeleting
-        ? Text('Car deleted')
-        : Text('Do you want to delete ${carService.activeCar.name}?');
+    return Text(
+      _isDeleting
+          ? 'Car deleted'
+          : 'Do you want to delete ${carService.activeCar.name}?',
+    );
   }
 
   Widget _buildDialogContent(void Function(void Function()) setState) {
-    return Container(
+    return SizedBox(
       width: 100,
-      height: 100,
-      color: Colors.orangeAccent,
+      height: 200,
+      child: Center(
+        child: Lottie.network(
+          "https://lottie.host/447b2753-b3ef-4163-a13c-f4adb5490135/2mxMx67iBP.json",
+          controller: _controller,
+          width: _ANIMATION_WIDTH,
+          height: _ANIMATION_HEIGHT,
+          onLoaded: (composition) {
+            _controller.duration = composition.duration;
+            _controller.animateTo(
+              1 / _controller.duration!.inSeconds,
+              duration: const Duration(seconds: 1),
+            );
+          },
+        ),
+      ),
     );
   }
 
   List<Widget> _buildDialogActions(
-      BuildContext context, void Function(void Function()) setState) {
+    BuildContext context,
+    void Function(void Function()) setState,
+  ) {
     if (_isDeleting) {
       return [];
-    } else {
-      return [
-        TextButton(
-          onPressed: () {
-            Navigator.of(context).pop();
-          },
-          child: Text('Cancel'),
-        ),
-        TextButton(
-          onPressed: () {
-            setState(() {
-              _isDeleting = true;
-              carService.deleteCar(carService.activeCar.id).listen((_) {});
-              Future.delayed(const Duration(seconds: 2), () {
-                _isDeleting = false;
-                Navigator.of(context).popUntil(
-                    (route) => route.settings.name == Routes.carNavigation);
-              });
-            });
-          },
-          child: Text('Delete'),
-        ),
-      ];
     }
+    return [
+      TextButton(
+        onPressed: () {
+          setState(() {
+            _controller.reset();
+          });
+          Navigator.of(context).pop();
+        },
+        child: const Text(
+          'Cancel',
+          style: TextStyle(color: Colors.black),
+        ),
+      ),
+      ElevatedButton(
+        onPressed: () {
+          setState(() {
+            _isDeleting = true;
+            _controller.duration = const Duration(seconds: 2);
+            _controller.forward();
+            carService.deleteCar(carService.activeCar.id).listen((_) {});
+            Future.delayed(const Duration(milliseconds: 1600), () {
+              Navigator.of(context).popUntil(
+                (route) => route.settings.name == Routes.carNavigation,
+              );
+            });
+          });
+        },
+        style: ElevatedButton.styleFrom(
+          backgroundColor: Colors.red,
+          textStyle: const TextStyle(color: Colors.black),
+        ),
+        child: const Text(
+          'Delete',
+          style: TextStyle(color: Colors.black),
+        ),
+      ),
+    ];
   }
 }
