02e4775db2c8710608cf4dcc3fe95273f9441115
adamgabry <adam.gabrys@seznam.cz>
2025-01-03 11:01:47 +0100

    refactor: NOTES - decomposed to widgets and added consts

--- lib/screens/car_notes/note_item/note_item.dart
+++ lib/screens/car_notes/note_item/note_item.dart
@@ -1,14 +1,20 @@
+import 'package:car_log/screens/car_notes/note_item/widget/edit_dialog.dart';
+import 'package:car_log/screens/car_notes/note_item/widget/note_content_widget.dart';
 import 'package:flutter/material.dart';
 import 'package:car_log/model/note.dart';
-import 'package:car_log/screens/car_notes/note_item/message_bubble.dart';
-import 'package:car_log/screens/car_notes/note_item/user_info_note_item.dart';
 import 'package:car_log/services/user_service.dart';
 import 'package:car_log/services/note_service.dart';
 import 'package:car_log/set_up_locator.dart';
 import 'package:swipe_to/swipe_to.dart';
-import 'package:car_log/screens/car_notes/reply_message_widget.dart';
 
-const _HORIZONTAL_MARGIN = EdgeInsets.symmetric(horizontal: 12.0);
+const _EDIT_TEXT = Text('Edit Note');
+const _DELETE_TEXT = Text('Delete Note');
+const _REPLY_TEXT = Text('Reply');
+const _SUCCESS_STATUS = 'success';
+const _NOTE_DELETED_SUCCESSFULLY = 'Note deleted successfully';
+const _ICON_DELETE = Icon(Icons.delete);
+const _ICON_EDIT = Icon(Icons.edit);
+const _ICON_REPLY = Icon(Icons.reply);
 
 class NoteItem extends StatelessWidget {
   final Note note;
@@ -26,99 +32,60 @@ class NoteItem extends StatelessWidget {
     final currentUser = get<UserService>().currentUser;
     final isCurrentUser = note.userId == currentUser?.id;
     final isAdmin = currentUser?.isAdmin ?? false;
+    final hasRightsToEditAndDelete = isCurrentUser || isAdmin;
     final noteService = get<NoteService>();
 
     return GestureDetector(
       onLongPress: () {
-        _showOptions(context, isCurrentUser, noteService, isAdmin);
+        _showOptions(context, noteService, hasRightsToEditAndDelete);
       },
       child: SwipeTo(
         onRightSwipe: !isCurrentUser
-            ? (details) {
-          if (onReply != null) {
-            onReply!(note);
+          ? (details) {
+          if (onReply != null) onReply!(note);
           }
-        }
-            : null,  // Disable right swipe for current user's messages
+          : null,
         onLeftSwipe: isCurrentUser
-            ? (details) {
-          if (onReply != null) {
-            onReply!(note);
+          ? (details) {
+          if (onReply != null) {onReply!(note);}
           }
-        }
-            : null,  // Disable left swipe for other users' messages
-        child: Align(
-          alignment: isCurrentUser ? Alignment.centerRight : Alignment.centerLeft,
-          child: Container(
-            margin: _HORIZONTAL_MARGIN,
-            decoration: BoxDecoration(
-              borderRadius: BorderRadius.circular(12),
-            ),
-            padding: const EdgeInsets.all(12.0),
-            child: Column(
-              crossAxisAlignment:
-              isCurrentUser ? CrossAxisAlignment.end : CrossAxisAlignment.start,
-              children: [
-                if (note.replyNoteContent != null) const SizedBox(height: 8),
-                if (note.replyNoteContent != null)
-                  ReplyMessageWidget(
-                    note: Note(
-                      userId: note.userId,
-                      content: note.replyNoteContent!,
-                      userName: note.userName,
-                    ),
-                    onCancelReply: () {},
-                  ),
-                UserInfoNoteItem(note: note, isCurrentUser: isCurrentUser),
-                const SizedBox(height: 4),
-                MessageBubble(note: note, isCurrentUser: isCurrentUser),
-              ],
-            ),
-          ),
-        ),
+          : null,
+        child: NoteContentWidget(note:note ,isCurrentUser: isCurrentUser),
       ),
     );
   }
 
   void _showOptions(
-      BuildContext context, bool isCurrentUser, NoteService noteService, bool isAdmin) {
+      BuildContext context, NoteService noteService, bool hasRightsToEditAndDelete) {
     showModalBottomSheet(
       context: context,
       builder: (context) {
         return Wrap(
           children: [
-            if (isCurrentUser || isAdmin)
+            if (hasRightsToEditAndDelete)
               ListTile(
-                leading: const Icon(Icons.edit),
-                title: const Text('Edit'),
+                leading: _ICON_EDIT,
+                title: _EDIT_TEXT,
                 onTap: () {
                   Navigator.pop(context);
-                  _editNote(context, noteService);
+                  _editNote(context, noteService, carId, note);
                 },
               ),
             ListTile(
-              leading: const Icon(Icons.reply),
-              title: const Text('Reply'),
+              leading: _ICON_REPLY,
+              title: _REPLY_TEXT,
               onTap: () {
                 Navigator.pop(context);
-                if (onReply != null) {
-                  onReply!(note);
-                }
+                if (onReply != null) onReply!(note);
               },
             ),
-            if (isCurrentUser || isAdmin)
+            if (hasRightsToEditAndDelete)
               ListTile(
-                leading: const Icon(Icons.delete),
-                title: const Text('Delete'),
+                leading: _ICON_DELETE,
+                title: _DELETE_TEXT,
                 onTap: () {
                   Navigator.pop(context);
-                  noteService.deleteNote(carId, note.id).listen((status) {
-                    if (status == 'success') {
-                      print('Note deleted successfully');
-                    } else {
-                      print('Failed to delete note: $status');
-                    }
-                  });
+                  _deleteNote(noteService, carId, note);
                 },
               ),
           ],
@@ -127,37 +94,22 @@ class NoteItem extends StatelessWidget {
     );
   }
 
-  void _editNote(BuildContext context, NoteService noteService) {
+  void _deleteNote(NoteService noteService, String carId, Note note) {
+    noteService.deleteNote(carId, note.id).listen((status) {
+      if (status == _SUCCESS_STATUS) {
+        print(_NOTE_DELETED_SUCCESSFULLY);
+      } else {
+        print('Failed to delete note: $status');
+      }
+    });
+  }
+
+  void _editNote(BuildContext context, NoteService noteService, String carId, Note note) {
     TextEditingController controller = TextEditingController(text: note.content);
     showDialog(
       context: context,
       builder: (context) {
-        return AlertDialog(
-          title: const Text('Edit Note'),
-          content: TextField(
-            controller: controller,
-            decoration: const InputDecoration(hintText: 'Edit your message'),
-          ),
-          actions: [
-            TextButton(
-              onPressed: () => Navigator.pop(context),
-              child: const Text('Cancel'),
-            ),
-            TextButton(
-              onPressed: () {
-                noteService.updateNote(
-                  carId,
-                  note.id,
-                  note.copyWith(content: controller.text),
-                ).listen((status) {
-                  print('Status: $status');
-                });
-                Navigator.pop(context);
-              },
-              child: const Text('Save'),
-            ),
-          ],
-        );
+        return buildEditDialog(context, controller, noteService, carId, note);
       },
     );
   }
