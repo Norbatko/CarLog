d35e25a627e550eb15f939d51f5d8e12cd4d4772
adamgabry <adam.gabrys@seznam.cz>
2025-01-06 12:27:38 +0100

    feat: ride update updates odometer + getting location on iOS

--- lib/services/ride_service.dart
+++ lib/services/ride_service.dart
@@ -9,14 +9,32 @@ class RideService {
 
   Stream<String> saveRide(Ride ride, String carId) async* {
     final ridesRef = carsCollection.doc(carId).collection('rides');
+    final carRef = carsCollection.doc(carId);
+    FirebaseFirestore firestore = FirebaseFirestore.instance; //for transaction
 
     try {
-      if (ride.id.isEmpty) {
-        DocumentReference newRideRef = await ridesRef.add(ride.toMap());
-        ride.id = newRideRef.id;
-      } else {
-        await ridesRef.doc(ride.id).set(ride.toMap());
-      }
+      await firestore.runTransaction((transaction) async {
+        DocumentSnapshot carSnapshot = await transaction.get(carRef);
+
+        if (!carSnapshot.exists) { throw Exception("Car not found");}
+
+        Map<String, dynamic> carData = carSnapshot['detail'];
+        String currentOdometerStatus = carData['odometer_status'] ?? '0';
+        int currentOdometer = int.tryParse(currentOdometerStatus) ?? 0;
+        int updatedOdometer = currentOdometer + ride.distance;
+
+        if (ride.id.isEmpty) {
+          DocumentReference newRideRef = ridesRef.doc();
+          ride.id = newRideRef.id;
+          transaction.set(newRideRef, ride.toMap());
+        } else {
+          transaction.set(ridesRef.doc(ride.id), ride.toMap());
+        }
+
+        transaction.update(carRef, {
+          'detail.odometer_status': updatedOdometer.toString(),
+        });
+      });
       yield 'success';
     } catch (e) {
       yield 'error: ${e.toString()}';
