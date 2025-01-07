93019e9745b053eb37404d4cc8b7ce471887641c
adamgabry <adam.gabrys@seznam.cz>
2025-01-07 19:56:23 +0100

    feat: ride map getting current location

--- lib/screens/ride_edit/ride_map/ride_map_selector.dart
+++ lib/screens/ride_edit/ride_map/ride_map_selector.dart
@@ -3,6 +3,8 @@ import 'package:flutter_map/flutter_map.dart' as flutterMap;
 import 'package:latlong2/latlong.dart';
 import 'package:car_log/services/location_service.dart';
 import 'package:car_log/set_up_locator.dart';
+import 'package:geolocator/geolocator.dart';
+
 class RideMapSelector extends StatefulWidget {
   final flutterMap.MapController mapController;
   final LatLng? initialPosition;
@@ -21,6 +23,25 @@ class RideMapSelector extends StatefulWidget {
 
 class _RideMapSelectorState extends State<RideMapSelector> {
   LatLng? _selectedPosition;
+  LatLng? _currentPosition;
+
+  @override
+  void initState() {
+    super.initState();
+    _fetchCurrentLocation();
+  }
+
+  Future<void> _fetchCurrentLocation() async {
+    try {
+      Position position = await Geolocator.getCurrentPosition();
+      setState(() {
+        _currentPosition = LatLng(position.latitude, position.longitude);
+        widget.mapController.move(_currentPosition!, 14.0);
+      });
+    } catch (e) {
+      _currentPosition = LatLng(49.210015, 16.598854);
+    }
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -55,6 +76,21 @@ class _RideMapSelectorState extends State<RideMapSelector> {
                         Icons.location_pin,
                         color: Colors.red,
                         size: 40,
+                      ),
+                    ),
+                  ],
+                ),
+              if (_currentPosition != null)
+                flutterMap.MarkerLayer(
+                  markers: [
+                    flutterMap.Marker(
+                      point: _currentPosition!,
+                      width: 45,
+                      height: 45,
+                      child: const Icon(
+                        Icons.my_location,
+                        color: Colors.blue,
+                        size: 35,
                       ),
                     ),
                   ],
