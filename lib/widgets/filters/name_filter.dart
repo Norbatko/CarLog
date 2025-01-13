601ac0ffca2d16ecd43a7830da0c84d347c51886
Tomáš Ebert <xebert@fi.muni.cz>
2025-01-13 10:14:27 +0100

    feat: Add hint text

--- lib/widgets/filters/name_filter.dart
+++ lib/widgets/filters/name_filter.dart
@@ -1,8 +1,10 @@
 import 'package:flutter/material.dart';
 
 class NameFilter extends StatefulWidget {
+  final String hintText;
   final void Function(String)? onChanged;
-  const NameFilter({super.key, required this.onChanged});
+  const NameFilter(
+      {super.key, required this.onChanged, required this.hintText});
 
   @override
   State<NameFilter> createState() => _NameFilterState();
@@ -20,6 +22,7 @@ class _NameFilterState extends State<NameFilter> {
         decoration: InputDecoration(
             prefixIcon: const Icon(Icons.search),
             labelText: "Search",
+            hintText: widget.hintText,
             border: OutlineInputBorder(
                 borderRadius: BorderRadius.circular(20),
                 borderSide: const BorderSide(
