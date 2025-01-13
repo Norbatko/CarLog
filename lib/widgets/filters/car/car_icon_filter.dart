3b23429de77c0a27db3f863b6ddcc772d343bf1f
Tomáš Ebert <xebert@fi.muni.cz>
2025-01-13 08:46:24 +0100

    feat: Create Car Icon filter widget

---
+++ lib/widgets/filters/car/car_icon_filter.dart
@@ -0,0 +1,63 @@
+import 'package:car_log/model/car.dart';
+import 'package:flutter/material.dart';
+
+class CarIconFilter extends StatelessWidget {
+  final Set<int> selectedIcons;
+  final ValueChanged<Set<int>> onSelectionChanged;
+  final List<Car> cars;
+
+  const CarIconFilter({
+    super.key,
+    required this.selectedIcons,
+    required this.onSelectionChanged,
+    required this.cars,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    final uniqueIconsCars = cars.map((car) => car.icon).toSet().toList();
+
+    return Column(
+      crossAxisAlignment: CrossAxisAlignment.start,
+      children: [
+        const Text('Car Icons'),
+        Wrap(
+          spacing: 8.0, // Space between chips
+          runSpacing: 4.0,
+          children: uniqueIconsCars.map((iconNumber) {
+            return _buildFilterChip(getCarIcon(iconNumber), iconNumber);
+          }).toList(),
+        ),
+      ],
+    );
+  }
+
+  Widget _buildFilterChip(IconData icon, int value) {
+    return FilterChip(
+      label: Icon(icon),
+      selected: selectedIcons.contains(value),
+      onSelected: (isSelected) {
+        final newSelection = Set<int>.from(selectedIcons);
+        if (isSelected) {
+          newSelection.add(value);
+        } else {
+          newSelection.remove(value);
+        }
+        onSelectionChanged(newSelection);
+      },
+    );
+  }
+
+  IconData getCarIcon(int icon) {
+    switch (icon) {
+      case 0:
+        return Icons.directions_car;
+      case 1:
+        return Icons.directions_bus;
+      case 2:
+        return Icons.local_shipping;
+      default:
+        return Icons.directions_car;
+    }
+  }
+}
